75a099915e96a6f0f5cea3aaa91aaaec94f88b83
Prafulla Diwesh <49725701+prafulladiwesh@users.noreply.github.com>
2020-05-26 18:02:17 +0530

    Updated for displaying result images

--- Python/PythonFile/AnotherPython.py
+++ Python/PythonFile/AnotherPython.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[69]:
-
-
 from sys import argv
 from sklearn.metrics.pairwise import cosine_similarity
 from PIL import Image
+from sklearn.decomposition import PCA
+import numpy as np
 import subprocess
 import json
 import os
@@ -35,6 +31,20 @@ query_feature = pd.read_csv('queryFeature.csv', header=None)
 # Dropping 1st column from the dataframe
 query_feature_vector = query_feature.drop(columns=[0])
 
+
+# Finding out the number of dimensions that keeps 95% of the variance of the original images
+pca_dims = PCA()
+pca_dims.fit(data_feature_vector)
+cumsum = np.cumsum(pca_dims.explained_variance_ratio_)
+d = np.argmax(cumsum >= 0.95) + 1
+
+# Applying PCA on data feature vector
+pca = PCA(n_components=d)
+data_feature_vector = pca.fit_transform(data_feature_vector)
+
+# Applying same PCA rule to query feature vector
+query_feature_vector = pca.transform(query_feature_vector)
+
 # cosine similarity between query image and complete dataset
 cos_sim = cosine_similarity(query_feature_vector, data_feature_vector).flatten()
 
@@ -45,6 +55,30 @@ dataset = pd.concat([data_features, cos_sim_df], axis=1)
 
 # sorting result in descending order
 sort_result = dataset.sort_values(by='cos_sim', ascending=False)[[0, 'cos_sim']].head(10)
+
+# Query Image
+print('QUERY IMAGE..')
+query_img = Image.open(query_feature[0][0])
+plt.figure(figsize=(5,5))
+plt.imshow(query_img, cmap=plt.cm.binary)
+plt.show()
+
+# Display result images
+img_array = []
+for i in sort_result.index:
+    img = Image.open(sort_result[0][i])
+    img_array.append(img)
+    
+# Top 10 Search Results
+print('TOP 10 SEARCH RESULT')
+plt.figure(figsize=(15,15))
+for i in range(len(img_array)):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(img_array[i], cmap=plt.cm.binary)
+#     plt.xlabel(img_array[i][1])
+plt.show()
 
 # converting result to json format
 json_array = []
@@ -60,4 +94,3 @@ for i in sort_result.index:
 # convert into JSON:
 json_data = json.dumps(json_array)
 print(json_data)
-
